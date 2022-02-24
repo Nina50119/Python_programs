71d5440f8a41bb6a659817f98f2c9b98724e86e5
Nina50119 <82484128+Nina50119@users.noreply.github.com>
2022-02-24 18:38:36 +0100

    password validity test

---
+++ cloops.py
@@ -0,0 +1,23 @@
+# python program to test the validity of  a program
+import re
+p=input("Enter your password")
+print(p)
+x=True
+while x:
+    if(len(p)<6  or len(p)>12):
+        break
+    elif not re.search("[A-Z]",p):
+        break
+    elif not re.search ("[0-9]",p):
+         break
+    elif not re.search("[$#@]",p):
+        break
+    elif re.search("\s",p):
+        break
+    else:
+        print("Valid password")
+        x=False
+        break
+
+if x:
+    print("Not a valid password")
